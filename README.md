b469cb033afa7566412066efa53522f5474f67bd
vidhan1995 <vidhan1995.jain@gmail.com>
2015-12-12 15:04:21 +0530

    Added Authentication of username and password

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Spoj-toolkit
+
 A command line tool to read the problems and submit solutions directly through terminal.
--- login.py
+++ login.py
@@ -1,5 +1,8 @@
 import sys
 import getpass
+import os
+import re
+from bs4 import BeautifulSoup
 
 try:
     import requests
@@ -7,16 +10,52 @@ except ImportError:
     print "Install requests"
     sys.exit(1)
 
-url = "http://www.spoj.com/login"
-username = raw_input("Enter Spoj username: ")
-password = getpass.getpass()
+def first_time_login():
+    name = raw_input("Enter your spoj username: ")
+    password = getpass.getpass()
+    with file('passd.txt', 'w') as f:
+        f.write(name + "\n" + password)
+    return name,password
 
-session = requests.session()
-
-login_data = {
-        'login_user' : username,
-        'password' : password,
-        'submit': 'login',
+def start_session(url, username, password):
+    print ('Verifing..')
+    with requests.Session() as session:
+        payload = {
+            'login_user' : username,
+            'password' : password,
+            'submit': 'login',
         }
+        r = session.post(url, data = payload)
 
-r = session.post(url, data = login_data)
+        html = r.text
+        html.encode('utf-8')
+        soup = BeautifulSoup(html, 'html.parser')
+        for auth in soup.find_all('h3'):
+            if (auth.get_text() == 'Authentication failed!'):
+                print ('Authentication Failed!')
+                print ('Enter login credentials!')
+                name,p = first_time_login()
+                start_session(url, name, p)
+                break
+        else:
+            print ('Authentication Passed!')
+
+
+def credentials(url):
+    if (os.path.isfile('passd.txt')):
+        f = open('passd.txt','r')
+        username,password = f.readlines()
+        username = username.strip()
+        password = password.strip()
+    else:
+        username,password = first_time_login()
+
+    start_session(url, username, password)
+
+def main():
+    url = "http://www.spoj.com/login"
+    credentials(url)
+
+
+if __name__ == '__main__':
+    main()
