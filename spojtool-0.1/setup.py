ab3e81aca5bd3764f2b74b26c7babb02a658590f
vidhan1995 <vidhan1995.jain@gmail.com>
2015-12-17 19:46:10 +0530

    Added travis integration and setup.py

---
+++ spojtool-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup ( name = 'spojtool',
+        version = '1.0',
+        description = 'View and submit solutions to the www.spoj.com using commandline',
+        url = 'https://github.com/vidhan13j07/Spoj-toolkit',
+        author = 'Vidhan Jain',
+        author_email = 'vidhanj1307@gmail.com',
+        license = 'MIT',
+        packages = ['spojtool'],
+        scripts = ['bin/spojtool'],
+        install_requires = [ 'BeautifulSoup4' ],
+        zip_false = False)
